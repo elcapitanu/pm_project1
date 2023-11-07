0f635feb6d143326c023c0218bf0953291501649
elcapitanu <bernardoacgabriel@gmail.com>
2023-11-07 22:52:58 +0000

    utils/data.py: added function to plot comparasion of data and moved normalize_rad to this file.

--- utils/data.py
+++ utils/data.py
@@ -1,5 +1,6 @@
 import numpy as np
 import re
+import matplotlib.pyplot as plt
 
 def extract(file_path):
     df = []
@@ -9,3 +10,16 @@ def extract(file_path):
             df.append(col)
 
     return np.array(df).astype(float)
+
+def normalize_rad(r):
+    if r > np.pi:
+        r -= 2 * np.pi
+    elif r < -np.pi:
+        r += 2 * np.pi
+    return r
+
+def show_comparasion(t, real, pred):
+    plt.scatter(t, real, marker='o', color='red', label='real')
+    plt.scatter(t, pred, marker='o', color='blue', label='pred')
+    plt.legend()
+    plt.show()
