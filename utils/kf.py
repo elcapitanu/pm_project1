8467157a487d8ce8307a16e67dd0a23efe2568d2
Bernardo Gabriel <bernardo.gabriel@lsts.pt>
2023-11-08 15:41:14 +0000

    data/kf.py: experiments with update phase.

--- utils/kf.py
+++ utils/kf.py
@@ -2,14 +2,17 @@ import numpy as np
 from numpy.linalg import inv
 
 def predict(X, A, B, U, P, Q):
-    # X = A @ X + B @ U
-    X = np.dot(A, X) + np.dot(B, U)
+    X = A @ X + B @ U
     P = A @ P @ A.T + Q
     return X, P
 
-def update(X, P, Y, H, R):
-    S = H @ P @ H.T + R
-    K = P @ H.T @ inv(S)
+def update(X, P, Z, H, R):
+    Y = Z - X
+    # S = H @ P @ H.T + R
+    # K = P @ H.T @ inv(S)
+    K = np.array([[0.01, 0, 0],
+                  [0, 0.01, 0],
+                  [0, 0, 0.01]])
     X = X + K @ Y
-    P = P - K @ S @ K.T
+    # P = P - K @ S @ K.T
     return X, P
