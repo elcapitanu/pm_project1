1f7e4ccb86f60d69286ecb73d83591a73e2f98a5
elcapitanu <bernardoacgabriel@gmail.com>
2023-11-10 10:14:20 +0000

    task3.py: fixed Q matrix, when to update and choose_landmark().

--- task3.py
+++ task3.py
@@ -5,8 +5,31 @@ from utils import video
 from utils import data
 from utils import kf
 
-def choose_landmark():
-    return True
+l1 = [0,0]
+l2 = [10,0]
+
+def choose_landmark(state):
+    psi1 = data.normalize_rad(np.arctan2(l1[1]-state[1],l1[0]-state[0])-state[2])
+    psi2 = data.normalize_rad(np.arctan2(l2[1]-state[1],l2[0]-state[0])-state[2])
+
+    diff_psi1 = abs(psi - psi1)
+    diff_psi2 = abs(psi - psi2)
+
+    dist_l1 = np.sqrt((state[0]-l1[0])**2+(state[1]-l1[1])**2)
+    dist_l2 = np.sqrt((state[0]-l2[0])**2+(state[1]-l2[1])**2)
+
+    if dist_l1 < dist_l2:
+      if(abs(diff_psi1) <= abs(diff_psi2)):
+        return l1
+      else:
+        return l2
+    else:
+      if(abs(diff_psi2) <= abs(diff_psi1)):
+        return l2
+      else:
+        return l1
+      
+    return 0
 
 #video
 vid = True
@@ -33,13 +56,10 @@ sigma_w = 0.05
 
 sigma_r = 0.5
 sigma_psi = 0.1
-l1 = [0,0]
-l2 = [10,0]
 
 x_pred = []
 y_pred = []
 theta_pred = []
-
 
 state = np.array([x0, y0, theta0])
 
@@ -47,14 +67,12 @@ A = np.array([[1, 0, 0],
               [0, 1, 0],
               [0, 0, 1]])
 
-P = np.eye(3)
+P = np.array([[1, 1, 1],
+              [1, 1, 1],
+              [1, 1, 1]])
 
-# Q = np.array([[sigma_v**2, 0],
-#               [0,          sigma_w**2]])
-
-Q = np.array([[sigma_v**2, 0,          0],
-              [0,          sigma_v**2, 0],
-              [0,          0,          sigma_w**2]])
+Q = np.array([[sigma_v**2, 0],
+              [0,          sigma_w**2]])
 
 R = np.array([[sigma_r**2, 0],
               [0,          sigma_psi**2]])
@@ -72,16 +90,15 @@ for i in range(len(df)):
                   [0,                     dt]])
     
     U = np.array([v, w])
-
+    
     state, P = kf.predict(state, A, B, U, P, Q, dt)
 
     # kalman filter update
-    if (r > 0 and psi > 0):
+    if (r != 0 and abs(psi) <= np.pi):
         Z = np.array([r, psi])
-        if (choose_landmark()):
-            state, P = kf.update(state, Z, P, R, l1)
-        else:
-            state, P = kf.update(state, Z, P, R, l2)
+        landmark = choose_landmark(state)
+        if (landmark):
+            state, P = kf.update(state, Z, P, R, landmark)
 
     x_pred.append(state[0])
     y_pred.append(state[1])
