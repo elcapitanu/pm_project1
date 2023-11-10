8e3038960ff420430802223c1f691c7f58f0d3c5
xBogas <119903183+xBogas@users.noreply.github.com>
2023-11-10 18:39:36 +0000

    task4.py: change state vector size to Nx1

--- task4.py
+++ task4.py
@@ -1,12 +1,9 @@
+from doctest import FAIL_FAST
 import numpy as np
 import matplotlib.pyplot as plt
 
 from utils import video
 from utils import data
-from utils import kf
-
-def mark_beacon():
-    return
 
 #video
 vid = False
@@ -40,21 +37,67 @@ y1_pred = []
 x2_pred = []
 y2_pred = []
 
-state = np.array([0, 0, 0])
+state = np.array([[0], [0], [0]])
 
-A = np.array([[1, 0, 0],
-              [0, 1, 0],
-              [0, 0, 1]])
-
-P = np.array([[1, 1, 1],
-              [1, 1, 1],
-              [1, 1, 1]])
+P = np.eye(3)
 
 Q = np.array([[sigma_v**2, 0],
               [0,          sigma_w**2]])
 
 R = np.array([[sigma_r**2, 0],
               [0,          sigma_psi**2]])
+
+from numpy.linalg import inv
+
+
+# save beacons [x, y, n] n times viewed
+known_beacons = []
+def new_beacon(state, r, psi):
+    
+    if len(state) <= 3:
+        return True
+
+    x, y, theta = state[:3].reshape(3)
+    bx = x + r*np.cos(theta + psi)
+    by = y + r*np.sin(theta + psi)
+
+    for i, beacon in enumerate(known_beacons):
+        dst = np.sqrt((beacon[0] - bx)** 2 + (beacon[1] - by)** 2)
+        if dst <= 10 :
+            n = beacon[2]
+            beacon[0] = (beacon[0] * n + bx)/(n+1)
+            beacon[1] = (beacon[1] * n + by)/(n+1)
+            beacon[2] = n+1
+            known_beacons[i] = beacon
+            return False
+
+    known_beacons.append([bx, by, 1])
+    return True
+
+def predict(X, U, P, Q, dt):
+    x, y, theta = X[:3].reshape(3)
+    v, omega = U.reshape(2)
+
+    theta_pred = data.normalize_rad(theta + omega * dt)
+    x_pred = x + v/omega * ( np.sin(theta_pred) - np.sin(theta))
+    y_pred = y + v/omega * (-np.cos(theta_pred) + np.cos(theta))
+
+    F_x = np.array([[1, 0, v/omega * (np.cos(theta_pred) - np.cos(theta))],
+                    [0, 1, v/omega * (np.sin(theta_pred) - np.sin(theta))],
+                    [0, 0, 1]])
+    
+    F_u = np.array([[(np.sin(theta_pred) - np.sin(theta))/omega, v/(omega**2) * (np.sin(theta_pred) - dt*omega*np.cos(theta_pred) - np.sin(theta))],
+                    [(np.cos(theta) - np.cos(theta_pred))/omega, v/(omega**2) * (np.cos(theta_pred) + dt*omega*np.sin(theta_pred) - np.cos(theta))],
+                    [0, dt]])
+
+    P = F_x @ P @ F_x.T + F_u @ Q @ F_u.T
+
+    X_pred = X
+    X_pred[0,0] = x_pred
+    X_pred[1,0] = y_pred
+    X_pred[2,0] = theta_pred
+    return X_pred, P
+
 
 for i in range(len(df)):
     t = df[i,0]
@@ -63,20 +106,16 @@ for i in range(len(df)):
     r = df[i,6]
     psi = df[i,7]
 
-    # kalman filter predict    
-    B = np.array([[dt * np.cos(state[2]), 0],
-                  [dt * np.sin(state[2]), 0],
-                  [0,                     dt]])
-    
-    U = np.array([v, w])
+    if new_beacon(state, r, psi) :
+        state = np.vstack((state, [[r], [psi]]))
 
-    state, P = kf.predict(state, A, B, U, P, Q, dt)
+    U = np.array([[v], [w]])
 
-    x_pred.append(state[0])
-    y_pred.append(state[1])
-    theta_pred.append(state[2])
+    state, P = predict(state, U, P, Q, dt)
 
-    mark_beacon()
+    x_pred.append(state[0, 0])
+    y_pred.append(state[1, 0])
+    theta_pred.append(state[2, 0])
 
     if vid:
         out = video.update(out, t, frame_width, frame_height, state, np.array([df[i,1],df[i,2],df[i,3]]), True, False)
@@ -84,11 +123,13 @@ for i in range(len(df)):
 if vid:
     video.export(out)
 
+exit()
+
 data.show_comparasion(df[:, 0], x_real, x_pred)
 data.show_comparasion(df[:, 0], y_real, y_pred)
 data.show_comparasion(df[:, 0], theta_real, theta_pred)
 
-plt.scatter(x_real, y_real, marker='.', color='red', label='real')
-plt.scatter(x_pred, y_pred, marker='.', color='blue', label='pred')
+plt.scatter(x_real, y_real, marker='.', color='red', label='real') # type: ignore
+plt.scatter(x_pred, y_pred, marker='.', color='blue', label='pred') # type: ignore
 plt.legend()
 plt.show()
