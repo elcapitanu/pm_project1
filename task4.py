0f12ac8ec85cb9340137f4c34ed87e9be6e2df7a
Bernardo Gabriel <bernardo.gabriel@lsts.pt>
2023-11-10 23:29:40 +0000

    task4.py: fixed algorithm.

--- task4.py
+++ task4.py
@@ -1,12 +1,43 @@
-from numpy.linalg import inv
 import numpy as np
+from numpy.linalg import inv
 import matplotlib.pyplot as plt
 
 from utils import video
 from utils import data
 
+def get_landmark(landmarks, all_landmarks, new_landmark):
+    n_landmark = -1
+    distances = []
+
+    for landmark in landmarks:
+        distance = np.sqrt((landmark[0] - new_landmark[0])** 2 + (landmark[1] - new_landmark[1])** 2)
+        distances.append(distance)
+
+    if (distances):
+        min_dist = min(distances)
+        if min_dist <= 12:
+            n_landmark = distances.index(min_dist)
+
+    # Known
+    if n_landmark != -1:
+        all_landmarks[n_landmark].append(new_landmark)
+        x_sum , y_sum = 0, 0
+        for landmark in all_landmarks[n_landmark]:
+            x_sum += landmark[0]
+            y_sum += landmark[1]
+
+        landmarks[n_landmark][0] = x_sum / len(all_landmarks[n_landmark])
+        landmarks[n_landmark][1] = y_sum / len(all_landmarks[n_landmark])
+
+    # New
+    else:
+        landmarks.append(new_landmark)
+        all_landmarks.append([new_landmark])
+
+    return landmarks, all_landmarks, n_landmark
+
 #video
-vid = True
+vid = False
 output_file = './videos/task4.avi'
 frame_rate = 120
 frame_width = 1920
@@ -21,135 +52,33 @@ y_real = df[:, 2]
 theta_real = df[:, 3]
 dt = df[1,0]
 
+state =  np.array([[0], [0], [0]])
+
+P = np.eye(3) * 1
+
 sigma_v = 0.5
 sigma_w = 0.05
 
+Q = np.array([[sigma_v**2, 0],
+              [0,          sigma_w**2]])
+
 sigma_r = 0.5
 sigma_psi = 0.1
+
+R = np.array([[sigma_r**2, 0],
+              [0,          sigma_psi**2]])
 
 x_pred = []
 y_pred = []
 theta_pred = []
 
-x1_pred = []
-y1_pred = []
+landmarks = []
+all_landmarks = []
 
-x2_pred = []
-y2_pred = []
-
-state = np.array([[x_real[0]],
-                  [y_real[0]],
-                  [theta_real[0]]])
-
-P = np.eye(3) * 999
-
-Q = np.array([[sigma_v**2, 0],
-              [0,          sigma_w**2]])
-
-R = np.array([[sigma_r**2, 0],
-              [0,          sigma_psi**2]])
-
-
-# save beacons [x, y, n] n times viewed
-known_beacons = []
-saved_beacons = []
-#!!!!!
-def get_beacon(state, r, psi):
-
-    x, y, theta = state[:3].reshape(3)
-    bx = x + r*np.cos(theta + psi)
-    by = y + r*np.sin(theta + psi)
-
-    if len(state) <= 3:
-        known_beacons.append([bx, by, 1])
-        return True, [bx, by, 0]
-
-    x, y, theta = state[:3].reshape(3)
-    bx = x + r*np.cos(theta + psi)
-    by = y + r*np.sin(theta + psi)
-
-    for i, beacon in enumerate(known_beacons):
-        dst = np.sqrt((beacon[0] - bx)** 2 + (beacon[1] - by)** 2)
-        if dst <= 12 :
-            n = beacon[2]
-            beacon[0] = (beacon[0] * n + bx)/(n+1)
-            beacon[1] = (beacon[1] * n + by)/(n+1)
-            beacon[2] = n+1
-            known_beacons[i] = beacon
-            return False, [beacon[0], beacon[1], i]
-
-    known_beacons.append([bx, by, 1])
-    id = len(known_beacons) - 1
-    print(id)
-    return True, [bx, by, id]
-
-def predict(X, U, P, Q, dt):
-    x, y, theta = X[:3].reshape(3)
-    v, omega = U.reshape(2)
-
-    theta_pred = data.normalize_rad(theta + omega * dt)
-    x_pred = x + v/omega * ( np.sin(theta_pred) - np.sin(theta))
-    y_pred = y + v/omega * (-np.cos(theta_pred) + np.cos(theta))
-
-    F_x = np.array([[1, 0, v/omega * (np.cos(theta_pred) - np.cos(theta))],
-                    [0, 1, v/omega * (np.sin(theta_pred) - np.sin(theta))],
-                    [0, 0, 1]])
-    
-    F_u = np.array([[(np.sin(theta_pred) - np.sin(theta))/omega, v/(omega**2) * (np.sin(theta_pred) - dt*omega*np.cos(theta_pred) - np.sin(theta))],
-                    [(np.cos(theta) - np.cos(theta_pred))/omega, v/(omega**2) * (np.cos(theta_pred) + dt*omega*np.sin(theta_pred) - np.cos(theta))],
-                    [0, dt]])
-
-    Fx = np.eye(len(X), len(X))
-    for line in range(F_x.shape[0]):
-        for column in range(F_x.shape[0]):
-            Fx[line][column] = F_x[line][column]
-
-    Fu = np.zeros((len(X), 2))
-    for line in range(F_u.shape[0]):
-        for column in range(F_u.shape[1]):
-            Fu[line][column] = F_u[line][column]
-
-    tmp = np.eye(len(X), len(X)) * 10
-    for line in range(P.shape[0]):
-        for column in range(P.shape[1]):
-            tmp[line][column] = P[line][column]
-    P = tmp
-
-    P = Fx @ P @ Fx.T + Fu @ Q @ Fu.T
-
-    X_pred = X
-    X_pred[0,0] = x_pred
-    X_pred[1,0] = y_pred
-    X_pred[2,0] = theta_pred
-    return X_pred, P
-
-
-def step(state, r, psi, beacon):
-
-    x, y, theta = state[:3].reshape(3)
-    z = np.array([[r, psi]])
-
-    norm = np.sqrt((x-beacon[0])**2 + (y-beacon[1])**2)
-
-    H_1 = np.array([[(x-beacon[0])/norm,      (y-beacon[1])/norm,     0],
-                  [-(y-beacon[1])/(norm**2),(x-beacon[0])/(norm**2),-1]])
-
-
-    H = np.zeros((2, len(state)))
-
-    for line in range(H_1.shape[0]):
-      for column in range(H_1.shape[1]):
-        H[line][column] = H_1[line][column]
-    H[0, 3+2*beacon[2]] = 1
-    H[1, 3+2*beacon[2]+1] = 1
-
-
-    h = np.array([[norm,
-                   np.arctan2(beacon[1]-y, beacon[0]-x)-theta]])
-
-    delta = (z-h).reshape(2, 1)
-
-    return delta, R, H
+P_m = np.zeros((5,5))
+F_x = np.zeros((3,3))
+F_u = np.zeros((3,2))
+state = np.zeros((3,1))
 
 for i in range(len(df)):
     t = df[i,0]
@@ -158,42 +87,126 @@ for i in range(len(df)):
     r = df[i,6]
     psi = df[i,7]
 
-    U = np.array([[v], [w]])
+    x, y, theta = state[:3].reshape(3)
 
-    if (r != 0) and (psi <= np.pi/4): 
-        new, beacon = get_beacon(state, r, psi)
-        if new :
+    state[:3] = np.array([[x + v/w * ( np.sin(theta + dt * w) - np.sin(theta))],
+                          [y + v/w * (-np.cos(theta + dt * w) + np.cos(theta))],
+                          [data.normalize_rad(theta + w *dt)]])
+
+    x_m, y_m, theta_m = state[:3].reshape(3)
+
+
+    F_x_aux = np.array([[1, 0, v/w * (np.cos(theta + dt * w) - np.cos(theta))],
+                        [0, 1, v/w * (np.sin(theta + dt * w) - np.sin(theta))],
+                        [0, 0, 1]])
+
+    F_u_aux = np.array([[( np.sin(theta + dt * w) - np.sin(theta))/w,  v/(w**2) * (np.sin(theta + dt * w) - dt * w * np.cos(theta + dt * w)- np.sin(theta))],
+                        [(-np.cos(theta + dt * w) + np.cos(theta))/w, v/(w**2) * (np.cos(theta + dt * w) + dt * w * np.sin(theta + dt * w)- np.cos(theta))],
+                        [0, dt]])
+
+    if (r != 0):
+        landmark_x = x_m + r * np.cos(theta_m + psi)
+        landmark_y = y_m + r * np.sin(theta_m + psi)
+
+        landmarks, all_landmarks, n_landmark = get_landmark(landmarks, all_landmarks, [landmark_x, landmark_y])
+
+        len_landmarks= len(landmarks)
+
+        H_aux = np.array([[ (x_m-landmark_x)/np.sqrt((x_m-landmark_x)**2+(y_m-landmark_y)**2),  (y_m-landmark_y)/np.sqrt((x_m-landmark_x)**2+(y_m-landmark_y)**2), 0],
+                        [-(y_m-landmark_y)/((landmark_x-x_m)**2+(y_m-landmark_y)**2),        -(landmark_x-x_m)/((landmark_x-x_m)**2+(y_m-landmark_y)**2),       -1]])
+
+        H = np.zeros((2, (3 + 2*len(landmarks))))
+        for line in range(H_aux.shape[0]):
+            for column in range(H_aux.shape[1]):
+                H[line][column] = H_aux[line][column]
+        H[0, 3 + 2*n_landmark] = 1
+        H[1, 3 + 2*n_landmark + 1] = 1
+
+        # Known landmark
+        if n_landmark != -1:
+            landmark_x = landmarks[n_landmark][0]
+            landmark_y = landmarks[n_landmark][1]
+
+            z_k = np.array([[r], [psi]])
+
+            P_m = F_x @ P @ F_x.T + F_u @ Q @ F_u.T
+
+            S_k = H @ P_m @ H.T + R
+
+            K_k = P_m @ H.T @ np.linalg.inv(S_k)
+
+            P = P_m - K_k @ S_k @ K_k.T
+
+            h_k = np.array([[np.sqrt((x_m-landmark_x)**2+(y_m-landmark_y)**2)],
+                            [data.normalize_rad(np.arctan2(landmark_y-y_m,landmark_x-x_m)-theta_m)]])
+
+            y = z_k - h_k
+
+            y[1] = data.normalize_rad(y[1])
+
+            state = state + (K_k @ y)
+
+        # New landmark
+        else:
+            F_x = np.eye(3 + 2*len(landmarks), 3 + 2*len(landmarks))
+            for line in range(F_x_aux.shape[0]):
+                for column in range(F_x_aux.shape[1]):
+                    F_x[line][column] = F_x_aux[line][column]
+
+            F_u = np.zeros((3 + 2*len(landmarks), 2))
+            for line in range(F_u_aux.shape[0]):
+                for column in range(F_u_aux.shape[1]):
+                    F_u[line][column] = F_u_aux[line][column]
+
+            P_new = np.eye(P.shape[0] + 2, P.shape[1] + 2) * 1
+            for line in range(P.shape[0]):
+                for column in range(P.shape[1]):
+                    P_new[line][column] = P[line][column]
+
+            P = P_new
+            
+            P_m = F_x @ P @ F_x.T + F_u @ Q @ F_u.T
+
+            z_k = np.array([[r], [psi]])
+
+            S_k = H @ P_m @ H.T + R
+
+            K_k = P_m @ H.T @ np.linalg.inv(S_k)
+
+            P = P_m - K_k @ S_k @ K_k.T
+
+            h_k = np.array([[np.sqrt((x_m-landmark_x)**2+(y_m-landmark_y)**2)],
+                            [data.normalize_rad(np.arctan2(landmark_y-y_m,landmark_x-x_m)-theta_m)]])
+
+            y = z_k - h_k
+
+            y[1] = data.normalize_rad(y[1])
+
             state = np.vstack((state, [[r], [psi]]))
 
-        state, P = predict(state, U, P, Q, dt)
-        delta, R, H = step(state, r, psi, beacon)
+            state = state + (K_k @ y)
 
-        K = P @ H.T @ inv(H @ P @ H.T + R)
-        P = P - K @ (H @ P @ H.T + R) @ K.T
-
-        state = state + K @ delta
-
-    # no beacon
+    # No landmark
     else:
-        state, P = predict(state, U, P, Q, dt)
+        P = F_x @ P @ F_x.T + F_u @ Q @ F_u.T
 
-    x_pred.append(state[0, 0])
-    y_pred.append(state[1, 0])
-    theta_pred.append(state[2, 0])
+    x_pred.append(state[0])
+    y_pred.append(state[1])
+    theta_pred.append(state[2])
 
     if vid:
-        out = video.update_2(out, t, frame_width, frame_height, state, np.array([df[i,1],df[i,2],df[i,3]]), True, True, known_beacons)
+        out = video.update(out, t, frame_width, frame_height, state, np.array([df[i,1],df[i,2],df[i,3]]), True, False)
 
 if vid:
     video.export(out)
-
-print(known_beacons)
 
 data.show_comparasion(df[:, 0], x_real, x_pred)
 data.show_comparasion(df[:, 0], y_real, y_pred)
 data.show_comparasion(df[:, 0], theta_real, theta_pred)
 
-plt.scatter(x_real, y_real, marker='.', color='red', label='real') # type: ignore
-plt.scatter(x_pred, y_pred, marker='.', color='blue', label='pred') # type: ignore
+for l in landmarks:
+    plt.scatter(l[0], l[1], marker='o', color='green')
+plt.scatter(x_real, y_real, marker='.', color='blue', label='real')
+plt.scatter(x_pred, y_pred, marker='.', color='red', label='pred')
 plt.legend()
 plt.show()
